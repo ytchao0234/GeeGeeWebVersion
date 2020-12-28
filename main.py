2d5b8c9bcad9186e516f12122ad164a4a9d2f478
ytchao0234 <nd68319@gmail.com>
2020-12-28 13:36:21 +0800

    redirect test

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect(url_for( 'home' ))
+    return redirect(url_for('home'))
 
 @app.route('/home')
 def home():
-    return render_template( 'mainPage.html' )
+    return render_template('./mainPage.html')
 
 if __name__ == '__main__':
     app.run()
