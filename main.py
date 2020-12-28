c936f7dc448c436b1146408e195f660b5b8860c9
ytchao0234 <nd68319@gmail.com>
2020-12-28 14:30:39 +0800

    redirect test ok

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__,template_folder='/templates', static_url_path='/static')
 
 @app.route('/')
 def index():
-    return render_template('mainPage.html')
+    return redirect(url_for('home'))
 
 @app.route('/home')
 def home():
     return render_template('mainPage.html')
+
+@app.route('/record')
+def record():
+    return render_template('record.html')
 
 if __name__ == '__main__':
     app.run()
